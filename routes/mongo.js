764380389381a1ffef6db8f9b39b3f72645d9ac8
PerfectXYZ <107462415+PerfectXYZ@users.noreply.github.com>
2022-07-16 21:43:49 +0900

    0715_bugFixed(chargerStatus)

--- routes/mongo.js
+++ routes/mongo.js
@@ -6,19 +6,11 @@ const moment = require('moment');
 const axios = require('axios')
 const dateutil = require('date-utils');
 const mongoClient = require('mongodb').MongoClient
-const spawn = require('child_process').spawn
-const exec = require('child_process').exec;
 let today = new Date();
 var now = today.toFormat("YYYYMMDDHH");
 const { PythonShell } = require("python-shell");
 const async = require("async");
-var mysql = require("mysql") ;
-// var connection = mysql.createConnection({
-//   host : 
-//   user : 'admin',
-//   password : 
-//   database : 'st_db'
-// })
+
 //let controller = require("/map")
 
 const lainfo = '';
@@ -55,11 +47,6 @@ router.get('/api/list', function(req, res, next) {
   console.log('/api/list function');
   var count=[{}];
   let num =[];
-  var input = '11710';
-
-  EvStation.find({"zscode":input},function(err,docs){
-
-    if(err) console.log('/api/list function::err');
 
 
       var template = `
@@ -77,7 +64,6 @@ router.get('/api/list', function(req, res, next) {
        //console.log('api/list function::',template);
       res.end(template);
 
-  });
 
 });
 
@@ -127,7 +113,7 @@ router.get('/getcode', function(req,res,next) {
               //   res.send(CircularJSON.stringify(body))
               // })
               //get 으로 Parameter 넘기기 start
-                 tmp_urls = "http://localhost:8000/get?" //본인의 ec2 localhost 주소
+                 tmp_urls = "http://54.215.35.186:8000/get?"
                  queryParams = "input="+result_zscode+"&stg_name="+result_name+"&stg_la="+result_la+"&stg_lo="+result_lo+"&stg_cnt="+result_prfcnt ;
 
                  urls = encodeURI(tmp_urls + queryParams)
@@ -160,199 +146,166 @@ router.get('/getcode', function(req,res,next) {
         });
       }
 });
-//sql test
-// router.get('/get/sql', function(req, res, next) {
-//   console.log('/Get function');
-//   db = req.db;
-//   var input = req.query.input;
-//   urls ="http://54.215.67.115:3000/stggetdata?input="+ req.query.input;
-//   console.log(urls);
-//   request(urls, {json:true}, (err,result,body) =>{
-//     if(err) {return console.log(err)}
-//     console.log(body);
-//     console.log(result);
-//
-//
-//   })
-// })
+
 
 //////////////////////////////////////////////
 
 router.get('/get', function(req, res, next) {
-  var count=[{}];
-  let num =[];
-  console.log('/Get function');
-  db = req.db;
-  var input = req.query.input;
-  var input_name =req.query.stg_name;
-  var input_la = req.query.stg_la;
-  var input_lo = req.query.stg_lo;
-  var input_cnt = req.query.stg_cnt;
+
+    console.log('/get ==>');
+    db = req.db;
+    var input = req.query.input;        //지역코드
+    var input_name =req.query.stg_name; //공연장 이름
+    var input_la = req.query.stg_la;    //공연장 위도
+    var input_lo = req.query.stg_lo;
+    var input_cnt = req.query.stg_cnt;  //공연장 공연개수
 
 
+   if(input=='') {
 
-  console.log("input::::",input)
- if(input=='') {
+        EvStation.findOne({},function(err,docs){
+          if(err) console.log('err');
+          res.send(docs);
+        });
+      } else {
 
-      EvStation.findOne({},function(err,docs){
-        if(err) console.log('err');
-        res.send(docs);
-      });
-    } else {
-      console.log("Draw_Map.py params:::",input,input_la,input_lo);
+        var template;
 
-      var template;
-      let options = {
-        scriptPath: "/data/node/evInfo",
-        args: [input,input_la,input_lo,input_name,input_cnt]
-      };
-      async.waterfall([
-            function(callback) {
-              PythonShell.run("Draw_Map.py", options, function(err, data) {
-                if (err) throw err;
-                console.log(data);
-                console.log(new Date());
-                callback(null,data)
+       var cLength = 0;
+       var count=[];
+
+        async.waterfall([
+          function (callback) {
+            EvStation.find({"zscode":input},function(err,docs){
+              if(err) console.log('err');
+
+              var firstflag = 1;
+
+              for(var i =0; i<docs.length; i++){
+                if(docs[i]['충전기상태'] == '2'){
+                  if(firstflag){
+                    count.push({충전소명 : docs[0]['충전소명'], 충전소ID : docs[0]['충전소ID'], 위도 : docs[0]['위도'],  경도 : docs[0]['경도'],
+                               충전기상태 : docs[0]['충전기상태'], 주소 :  docs[0]['주소'], 전화번호 : docs[0]['전화번호'], 안내 : '1' });
+
+                    firstflag = 0;
+                    cLength ++;
+                  }
+
+                  //count 배열 내 같은 충전소명 있으면 충전가능대수만 1 추가
+                  //count 배열 내 같은 충전소명 없으면 장소명 추가
+
+                  for(var j=0; j<cLength; j++){
+                    if(count[j]['충전소ID'] !=docs[i]['충전소ID']){
+
+                      if(j == cLength-1){
+                        //console.log("마지막count:: 충전소명 없으면 장소명 추가:", count[j]['충전소ID'], " != ",docs[i]['충전소ID']  )
+                        count.push({충전소명 : docs[i]['충전소명'], 충전소ID : docs[i]['충전소ID'],  위도 : docs[i]['위도'],  경도 : docs[i]['경도'],
+                        충전기상태 : docs[i]['충전기상태'],주소 :  docs[i]['주소'], 전화번호 : docs[i]['전화번호'], 안내 : '1' });
+                        cLength ++;
+                      }
+                    }
+                    else {
+                      //console.log("count 배열 내 같은 충전소명 있으면 충전가능대수만 1 추가:" )
+                      //console.log(":::::::",count[j]['충전기상태'])
+                      let temp=parseInt(count[j]['안내'])
+                      count[j]['안내'] = temp +1;
+                      console.log("count[",j,"]['안내']::::" , count[j]['안내'])
+                      break;
+                    }
+                  }
+
+                }
+              }
+
+              callback(null)
             });
+
           },
-            function(data,callback) {
-              console.log("function(data,callback)");
-              template = `
-              <!doctype html>
-              <html>
-              <head>
-                <title>Result</title>
-                <meta charset="utf-8">
-              </head>
-              <body>
-                <table border = "1" margin:auto; text-align:center;>
+              function(callback) {
+
+                let options = {
+                  scriptPath: "/data/node/evInfo",
+                  args: [input,input_la,input_lo,input_name,input_cnt,JSON.stringify(count)]
+                };
+                PythonShell.run("Draw_Map.py", options, function(err, data) {
+                  if (err) throw err;
+                  console.log(data);
+                  console.log(new Date());
+                  callback(null,data)
+              });
+            },
+              function(data,callback) {
+                console.log("function(data,callback)");
+                template = `
+                <!doctype html>
+                <html>
+                <head>
+                  <title>Result</title>
+                  <meta charset="utf-8">
+                </head>
+                <body>
+                  <table border = "1" margin:auto; text-align:center;>
 
 
-               <tr>
-                 <iframe align = "center" name="targetURL1" class="iframe-preview center1" width="1000px" height="500px" src="./map_station2.html"></iframe>
-               </tr>
+                 <tr>
+                   <iframe align = "center" name="targetURL1" class="iframe-preview center1" width="1000px" height="500px" src="./map_station2.html"></iframe>
+                 </tr>
 
-             `;
-
-             EvStation.find({"zscode":input},function(err,docs){
-
-               if(err) console.log('err');
-
-                 template += `
                   <table border = "1" width = 1000px;>
                    <tr>
                    <th>충전소명</th>
-                   <th>충전기ID</th>
+                   <th>충전소ID</th>
                    <th>충전가능 대수</th>
                    <th>주소</th>
                    <th>전화번호</th>
                    </tr>
                    `;
 
-                   var firstflag = 1;
-                   var cLength = 0;
-                   for(var i =0; i<docs.length; i++){
-                     if(docs[i]['충전기상태'] == '2'){
-                       if(firstflag){
-                         count[0] = docs[0]
-                         firstflag = 0;
-                         cLength ++;
-                       }
-                       // if(!count.inclues(docs[i]['충전소ID'])){
-                       //   count.push(item))
-                       // }
-
-
-                       //count 배열 내 같은 충전소명 있으면 충전가능대수만 1 추가
-                       //count 배열 내 같은 충전소명 없으면 장소명 추가
-                       console.log("count.length ::", cLength)
-                       for(var j=0; j<cLength; j++){
-                         if(count[j]['충전소ID'] !=docs[i]['충전소ID']){
-
-                           if(j == cLength-1){
-                             console.log("마지막count:: 충전소명 없으면 장소명 추가:", count[j]['충전소ID'], " != ",docs[i]['충전소ID']  )
-                             count[j+1] = docs[i]
-                             count[j+1]['충전기상태'] = 1;
-                             cLength ++;
-                             console.log("충전소명 없으면 장소명 추가:", count[j+1]['충전소ID'],count[j+1]['충전기상태'])
-
-                           }
-                         }
-                         else {
-                           console.log("count 배열 내 같은 충전소명 있으면 충전가능대수만 1 추가:" )
-                           // num[j] = count[j]['충전기상태']
-                           console.log(":::::::",count[j]['충전기상태'])
-                           let temp=parseInt(count[j]['충전기상태'])
-                           count[j]['충전기상태'] = temp +1;
-                           // count[j]['충전기상태'] =count[j]['충전기상태']+1;
-                           console.log(":::::::",count[j]['충전기상태'])
-                           break;
-                         }
-
-
-
-                       }
-
-
+                     for (var i =0; i<cLength; i++){
+                       template += `
+                         <tr>
+                         <th>${count[i]['충전소명']}</th>
+                         <th>${count[i]['충전소ID']}</th>
+                         <th>${count[i]['안내']}</th>
+                         <th>${count[i]['주소']}</th>
+                         <th>${count[i]['전화번호']}</th>
+                         </tr>
+                         `;
                      }
 
-                   }
-
-                   for (var i =0; i<count.length; i++){
                      template += `
-                       <tr>
-                       <th>${count[i]['충전소명']}</th>
-                       <th>${count[i]['충전기ID']}</th>
-                       <th>${count[i]['충전기상태']}</th>
-                       <th>${count[i]['주소']}</th>
-                       <th>${count[i]['전화번호']}</th>
-                       </tr>
-                       `;
-                   }
+                     </table>
+                     </table>
+                    </body>
+                    </html>
+                   `;
+                   //console.log("/Get function::",template);
+                   res.end(template);
 
-                   template += `
-                   </table>
-                   </table>
-                  </body>
-                  </html>
-                 `;
-                 //console.log("/Get function::",template);
-                 res.end(template);
 
-             });
 
-              console.log(new Date());
-          callback(null)
-       }],
-       function (err) {
-         if (err){
-           console.log(err)
-           return;
+                console.log(new Date());
+            callback(null)
+         }],
+         function (err) {
+           if (err){
+             console.log(err)
+             return;
+           }
+           console.log("/Get function::draw map sync completed")
          }
-         console.log("/Get function::map async completed")
-       }
-    );
+      );
 
 
-      }
+        }
 });
 module.exports = router;
 
 
-//10분에 한번씩 실행할 방법?
-//cron 으로 돌리니 자꾸 server가 죽음 why?
-/*
-const resultexec1 = exec('python Get_evChargerStatusInfo.py');
-console.log ("python Get_evChargerStatusInfo.py")
-resultexec1.stdout.on('data', function(ls_result){
-    console.log(ls_result.toString());
-})
-*/
-
 //10분에 한번씩 UpdateMongoDB하기 (feat. Get_evChargerStatusInfo.py)
+//실시간 data 업데이트
 //cron 으로 돌리니 자꾸 server가 죽음 ..
-//8분간격 (딜레이보장안되니 10분했을때 딜레이생기면 update안되는 항목 생길수있음)
-//var timer = setInterval(UpdateMongoDB,480000);
+
 var timer =
 setInterval(UpdateMongoDB,600000);
 
