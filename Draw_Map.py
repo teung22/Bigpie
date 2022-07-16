4026ad20e204df42cd2f164657cb41eb8bdfa910
PerfectXYZ <107462415+PerfectXYZ@users.noreply.github.com>
2022-07-16 21:44:46 +0900

    0715_bugFixed(chargerStatus)

--- Draw_Map.py
+++ Draw_Map.py
@@ -27,19 +27,17 @@ import requests
 # def index(name=""):
 
 def main():
-    #1. 현재 위치 가져오기 (기능정의서 #2,3)
-    ############################################
-    #kakao Geo api로 내 위치 가져오기(?)
-    #필수: lat, lng, zcode(시), zscode(구)
-    #for test :: 고정
+
     input = sys.argv[1]
     latitude =sys.argv[2] # 위도
     longitude = sys.argv[3] # 경도
     input_name = sys.argv[4]
     input_cnt = sys.argv[5]
+    availableCount = json.loads(sys.argv[6])
+    #availableCount = json.loads(json.dumps(sys.argv[6]), ensure_ascii=True)
     # #bottle
     # name = input
-    print("11111: ", latitude, longitude)
+
     if int(input_cnt) >= 3:
         map = folium.Map(location=[latitude, longitude], zoom_start=15)
         iframe = folium.IFrame('오늘 총 ' + '<b>'+str(input_cnt)+'</b>'+'개의 공연이 있어 혼잡이 예상됩니다.'+'<br>'+' 여유로운 충전소를 찾으세요!', width = 350, height = 80)
@@ -51,34 +49,26 @@ def main():
         popup = folium.Popup(iframe, max_width=350)
         folium.Marker(location=[latitude, longitude], icon=folium.Icon(icon='star',color='orange'), tooltip = input_name, popup = popup).add_to(map)
 
-    #2. 데이터 가져오기 from MongoDB
-    ############################################
-    #for test : df로 가져오기 from Get_evStationInfo.py
 
-    connect_to = pymongo.MongoClient("3.36.89.204",27017)
-    mongodb = connect_to.bigpie.evstations
-    print("Draw_Map.py::mongodb.find({'zscode' : ", input)
-
-    for i in mongodb.find({'zscode' : input}):
-
-
+    for i in availableCount:
         lat = i['위도']
         long= i['경도']
 
-        if int(i['충전기상태']) >= 5:
-            iframe = folium.IFrame('<b>충전소명</b> : ' + str(i['충전소명']) + '<br><br>' + '<b>주소</b> : ' + str(i['주소']) + '<br><br>'+ '<b>충전가능 대수</b> : ' + str(i['충전기상태'])+'대'+ '<br><br>' +'<b>전화번호</b> : ' +str(i['전화번호']))
+        print("LAT & LONG", lat, long)
+        if int(i['안내']) >= 5:
+            iframe = folium.IFrame('<b>충전소명</b> : ' + str(i['충전소명']) + '<br><br>' + '<b>주소</b> : ' + str(i['주소']) + '<br><br>'+ '<b>충전가능 대수</b> : ' + str(i['안내'])+'대'+ '<br><br>' +'<b>전화번호</b> : ' +str(i['전화번호']))
             popup = folium.Popup(iframe, min_width=300, max_width=350)
-            folium.Marker(location=[lat, long], icon=folium.Icon(icon='flag',color='darkblue'), tooltip= str(i['충전기상태'])+'대', popup=popup).add_to(map)
+            folium.Marker(location=[lat, long], icon=folium.Icon(icon='flag',color='darkblue'), tooltip= str(i['안내'])+'대', popup=popup).add_to(map)
 
-        elif int(i['충전기상태'])>= 3:
-            iframe = folium.IFrame('<b>충전소명</b> : ' + str(i['충전소명']) + '<br><br>' + '<b>주소</b> : ' + str(i['주소']) + '<br><br>'+ '<b>충전가능 대수</b> : ' + str(i['충전기상태'])+'대'+ '<br><br>' +'<b>전화번호</b> : ' +str(i['전화번호']))
+        elif int(i['안내'])>= 3:
+            iframe = folium.IFrame('<b>충전소명</b> : ' + str(i['충전소명']) + '<br><br>' + '<b>주소</b> : ' + str(i['주소']) + '<br><br>'+ '<b>충전가능 대수</b> : ' + str(i['안내'])+'대'+ '<br><br>' +'<b>전화번호</b> : ' +str(i['전화번호']))
             popup = folium.Popup(iframe, min_width=300, max_width=350)
-            folium.Marker(location=[lat, long], icon=folium.Icon(icon='flag',color='blue'), tooltip=str(i['충전기상태'])+'대', popup=popup).add_to(map)
+            folium.Marker(location=[lat, long], icon=folium.Icon(icon='flag',color='blue'), tooltip=str(i['안내'])+'대', popup=popup).add_to(map)
 
         else:
-            iframe = folium.IFrame('<b>충전소명</b> : ' + str(i['충전소명']) + '<br><br>' + '<b>주소</b> : ' + str(i['주소']) + '<br><br>'+ '<b>충전가능 대수</b> : ' + str(i['충전기상태'])+'대'+ '<br><br>' +'<b>전화번호</b> : ' +str(i['전화번호']))
+            iframe = folium.IFrame('<b>충전소명</b> : ' + str(i['충전소명']) + '<br><br>' + '<b>주소</b> : ' + str(i['주소']) + '<br><br>'+ '<b>충전가능 대수</b> : ' + str(i['안내'])+'대'+ '<br><br>' +'<b>전화번호</b> : ' +str(i['전화번호']))
             popup = folium.Popup(iframe, min_width=300, max_width=350)
-            folium.Marker(location=[lat, long], icon=folium.Icon(icon='flag',color='lightblue'), tooltip=str(i['충전기상태'])+'대', popup=popup).add_to(map)
+            folium.Marker(location=[lat, long], icon=folium.Icon(icon='flag',color='lightblue'), tooltip=str(i['안내'])+'대', popup=popup).add_to(map)
 
 
 
