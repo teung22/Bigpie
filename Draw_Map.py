cd9e2024f9b472c295a2180d3b26974659f80af4
teung22 <107603268+teung22@users.noreply.github.com>
2022-07-21 12:00:45 +0900

    Update Draw_Map.py

--- Draw_Map.py
+++ Draw_Map.py
@@ -38,26 +38,26 @@ def main():
     input_name = sys.argv[4]
     input_cnt = sys.argv[5]
     availableCount = json.loads(sys.argv[6])
-    input_stgid = sys.argv[7]  # 0720 stg_id :공연시설 ID
-    chargerTypeInfo = json.loads(sys.argv[8])  # 0720 충전기타입 정보
+    input_stgid = sys.argv[7] # 0720 stg_id :공연시설 ID
+    chargerTypeInfo = json.loads(sys.argv[8])  # 충전타입
     #availableCount = json.loads(json.dumps(sys.argv[6]), ensure_ascii=True)
     # #bottle
     # name = input
-
-    #0720 공연정보API :: IP고정
+     #0720 공연정보API :: IP고정
     input_stgid = functrim(input_stgid) #0720 ' 제거
     iframe="<iframe width='350' height=300 src='http://3.38.37.58:3000/getPrfInfo?input="+input_stgid+"&cnt="+input_cnt+"' title='공연정보' frameborder=0 allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>"
 
+
     if int(input_cnt) >= 3:
         map = folium.Map(location=[latitude, longitude], zoom_start=15)
-       # iframe = folium.IFrame('오늘 총 ' + '<b>'+str(input_cnt)+'</b>'+'개의 공연이 있어 혼잡이 예상됩니다.'+'<br>'+' 여유로운 충전소를 찾으세요!', width = 350, height = 80)
+        #iframe = folium.IFrame('오늘 총 ' + '<b>'+str(input_cnt)+'</b>'+'개의 공연이 있어 혼잡이 예상됩니다.'+'<br>'+' 여유로운 충전소를 찾으세요!', width = 350, height = 80)
         popup = folium.Popup(iframe, max_width=350)
         folium.Marker(location=[latitude, longitude], icon=folium.Icon(icon='star',color='red'), tooltip = input_name, popup = popup ).add_to(map)
     else:
         map = folium.Map(location=[latitude, longitude], zoom_start=16)
-       # iframe = folium.IFrame('오늘 총 ' + '<b>'+str(input_cnt)+'</b>'+'개의 공연이 있습니다.' ,width = 230, height = 80)
+        #iframe = folium.IFrame('오늘 총 ' + '<b>'+str(input_cnt)+'</b>'+'개의 공연이 있습니다.' ,width = 230, height = 80)
         popup = folium.Popup(iframe, max_width=350)
-        folium.Marker(location=[latitude, longitude], icon=folium.Icon(icon='star',color='orange'), tooltip = input_name, popup = popup).add_to(map)
+        folium.Marker(location=[latitude, longitude], icon=folium.Icon(icon='star',color='pink'), tooltip = input_name, popup = popup).add_to(map)
 
     temp = ''
 
@@ -123,19 +123,19 @@ def main():
 
         if int(i['안내']) >= 5:
           # iframe = folium.IFrame('<b>충전소명</b> : ' + str(i['충전소명']) + '<br><br>' + '<b>주소</b> : ' + str(i['주소']) + '<br><br>'+ '<b>충전가능 대수</b> : ' + str(i['안내'])+'대'+ '<br><br>' +'<b>전화번호</b> : ' +str(i['전화번호']) + '<br><br>' +'<b>사용시간</b> : ' +str(i['사용시간']) + '<br><br>'+'<b>주차료</b> : '+ isFree, height = 260)
-            iframe = folium.IFrame(template, height =200)
+            iframe = folium.IFrame(template, height =220)
             popup = folium.Popup(iframe, min_width=320,max_width=400)
             folium.Marker(location=[lat, long], icon=folium.Icon(icon='flag',color='darkblue'), tooltip= str(i['안내'])+'대', popup=popup).add_to(map)
 
         elif int(i['안내'])>= 3:
           #  iframe = folium.IFrame('<b>충전소명</b> : ' + str(i['충전소명']) + '<br><br>' + '<b>주소</b> : ' + str(i['주소']) + '<br><br>'+ '<b>충전가능 대수</b> : ' + str(i['안내'])+'대'+ '<br><br>' +'<b>전화번호</b> : ' +str(i['전화번호']) + '<br><br>' +'<b>사용시간</b> : ' +str(i['사용시간']) + '<br><br>' +'<b>주차료</b> : '+ isFree, height = 260)
-            iframe = folium.IFrame(template, height =200)
+            iframe = folium.IFrame(template, height =220)
             popup = folium.Popup(iframe, min_width=320, max_width=400)
             folium.Marker(location=[lat, long], icon=folium.Icon(icon='flag',color='blue'), tooltip=str(i['안내'])+'대', popup=popup).add_to(map)
 
         else:
           #  iframe = folium.IFrame('<b>충전소명</b> : ' + str(i['충전소명']) + '<br><br>' + '<b>주소</b> : ' + str(i['주소']) + '<br><br>'+ '<b>충전가능 대수</b> : ' + str(i['안내'])+'대'+ '<br><br>' +'<b>전화번호</b> : ' +str(i['전화번호']) + '<br><br>' +'<b>사용시간</b> : ' +str(i['사용시간']) + '<br><br>'+'<b>주차료</b> : '+ isFree, height = 260)
-            iframe = folium.IFrame(template, height =200)
+            iframe = folium.IFrame(template, height =220)
             popup = folium.Popup(iframe, min_width=320, max_width=400)
             folium.Marker(location=[lat, long], icon=folium.Icon(icon='flag',color='lightblue'), tooltip=str(i['안내'])+'대', popup=popup).add_to(map)
 
